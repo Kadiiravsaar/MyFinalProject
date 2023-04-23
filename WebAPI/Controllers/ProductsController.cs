4772cd91e39e8b952bb39abc5ee91cb40caef32f
kadiiravsaar <kadiiravsar@gmail.com>
2023-04-23 21:37:43 +0300

    WebApi added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Buisness.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+           var products = _productService.GetAll();
+            if (products.Success)
+            {
+                return Ok(products);
+
+            }
+            return BadRequest(products);
+        }
+    }
+}
